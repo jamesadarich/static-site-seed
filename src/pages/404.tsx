5c1ebd7943c848ac2ad91abef5c7b683645c6f61
James Adarich <jadarich@neworbit.co.uk>
2018-12-08 23:19:12 +0000

    refactor http error status pages to functions

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,14 +1,12 @@
 import * as React from "react";
 import { StatusPage } from "../layouts/status-page";
 
-export default class NotFoundPage extends React.PureComponent {
-  public render() {
-    return (
-      <StatusPage
-        title="Not Found"
-        shortDescription="Page could not be found"
-        description="We couldn't find what you were looking for."
-      />
-    );
-  }
+export default function NotFoundPage() {
+  return (
+    <StatusPage
+      title="Not Found"
+      shortDescription="Page could not be found"
+      description="We couldn't find what you were looking for."
+    />
+  );
 }
--- src/pages/500.tsx
+++ src/pages/500.tsx
@@ -1,14 +1,12 @@
 import * as React from "react";
 import { StatusPage } from "../layouts/status-page";
 
-export default class UnexpectedErrorPage extends React.PureComponent {
-  public render() {
-    return (
-      <StatusPage
-        title="Unexpected Error"
-        shortDescription="An unexpected error ocurred"
-        description="Yikes, something really went wrong."
-      />
-    );
-  }
+export default function UnexpectedErrorPage() {
+  return (
+    <StatusPage
+      title="Unexpected Error"
+      shortDescription="An unexpected error ocurred"
+      description="Yikes, something really went wrong."
+    />
+  );
 }
