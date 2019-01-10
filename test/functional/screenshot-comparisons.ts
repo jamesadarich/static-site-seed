fb5e92685ff60c89b77da80e390fed1e28461a3b
James Adarich <jadarich@neworbit.co.uk>
2019-01-10 22:33:26 +0000

    fix linting issues

--- test/functional/screenshot-comparisons.ts
+++ test/functional/screenshot-comparisons.ts
@@ -1,4 +1,13 @@
-import { AsyncTest, Expect, Timeout, TestFixture, SetupFixture, AsyncSetupFixture, AsyncTeardownFixture, TestCase } from "alsatian";
+import {
+  AsyncTest,
+  Expect,
+  Timeout,
+  TestFixture,
+  SetupFixture,
+  AsyncSetupFixture,
+  AsyncTeardownFixture,
+  TestCase
+} from "alsatian";
 import * as puppeteer from "puppeteer";
 import pixelmatch = require("pixelmatch");
 import { PNG } from "pngjs";
@@ -8,78 +17,78 @@ import { app } from "../../server/app";
 import { Server } from "http";
 
 async function loadPng(path: string) {
-    path = join(__dirname, path);
+  path = join(__dirname, path);
 
-    return new Promise<PNG>((resolve, reject) => {
-        const png = createReadStream(path)
-                        .pipe(new PNG());
+  return new Promise<PNG>((resolve, reject) => {
+    const png = createReadStream(path).pipe(new PNG());
 
-        png.on("parsed", () => resolve(png));
-        png.on("error", reject);
-    });
+    png.on("parsed", () => resolve(png));
+    png.on("error", reject);
+  });
 }
 
 async function writePng(path: string, png: PNG) {
-    return new Promise((resolve, reject) => {
-        png.pack()
-           .pipe(createWriteStream(path))
-           .on("finish", resolve)
-           .on("error", reject);
-    });
+  return new Promise((resolve, reject) => {
+    png
+      .pack()
+      .pipe(createWriteStream(path))
+      .on("finish", resolve)
+      .on("error", reject);
+  });
 }
 
 @TestFixture("screenshot comparisons")
 export class ScreenshotComparisonTests {
+  private _port = 1234;
+  private _app: Server;
 
-    private _port = 1234;
-    private _app: Server;
+  @AsyncTest("desktop")
+  @Timeout(5000)
+  @TestCase("/", "home-page")
+  @TestCase("/blog", "blog-page")
+  public async desktop(path: string, pageName: string) {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
 
-    @AsyncSetupFixture
-    private _startServer() {
-        return new Promise(resolve => {            
-            this._app = app.listen(this._port, resolve);
-        });
-    }
+    await page.goto(`http://localhost:${this._port}${path}`);
 
+    await page.screenshot({
+      path: join(__dirname, `./${pageName}.actual.png`)
+    });
 
-    @AsyncTeardownFixture
-    private _closeServer() {
-        return new Promise(resolve => { 
-            this._app.close(resolve);
-        });
-    }
+    const expectedPng = await loadPng(`./${pageName}.expected.png`);
+    const actualPng = await loadPng(`./${pageName}.actual.png`);
 
-    @AsyncTest("desktop")
-    @Timeout(5000)
-    @TestCase("/", "home-page")
-    @TestCase("/blog", "blog-page")
-    public async desktop(path: string, pageName: string) {
-        const browser = await puppeteer.launch();
-        const page = await browser.newPage();
+    const diffPng = new PNG({
+      width: expectedPng.width,
+      height: expectedPng.height
+    });
 
-        await page.goto(`http://localhost:${this._port}${path}`);
+    const diffPixels = pixelmatch(
+      expectedPng.data,
+      actualPng.data,
+      diffPng.data,
+      expectedPng.width,
+      expectedPng.height,
+      { threshold: 0.1 }
+    );
 
-        await page.screenshot({ path: join(__dirname, `./${pageName}.actual.png`) });
+    await writePng(join(__dirname, `${pageName}.diff.png`), diffPng);
 
-        const expectedPng = await loadPng(`./${pageName}.expected.png`);
-        const actualPng = await loadPng(`./${pageName}.actual.png`);
+    Expect(diffPixels).toBe(0);
+  }
 
-        const diffPng = new PNG({
-            width: expectedPng.width,
-            height: expectedPng.height
-        });
+  @AsyncSetupFixture
+  private _startServer() {
+    return new Promise(resolve => {
+      this._app = app.listen(this._port, resolve);
+    });
+  }
 
-        const diffPixels = pixelmatch(
-            expectedPng.data,
-            actualPng.data,
-            diffPng.data,
-            expectedPng.width,
-            expectedPng.height,
-            { threshold: 0.1 }
-        );
-
-        await writePng(join(__dirname, `${pageName}.diff.png`), diffPng);
-
-        Expect(diffPixels).toBe(0);
-    }
+  @AsyncTeardownFixture
+  private _closeServer() {
+    return new Promise(resolve => {
+      this._app.close(resolve);
+    });
+  }
 }
